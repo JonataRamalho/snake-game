679f4a91d338dae5d915deda8dad2d9724e97224
JonataRamalho <jonataramalho.s@gmail.com>
2020-12-21 21:41:56 -0300

    feat: define movimentos da cobra

--- script.js
+++ script.js
@@ -6,18 +6,45 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
-function createBackground() {
+function createBackground(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 8, 16 * box, 16 * box);
 }
 
-function createSnake() {
+function createSnake(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
-createBackground();
-createSnake();
+function startGame(){
+    createBackground();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right"){
+        snakeX += box;
+    } else if(direction == "left"){
+        snakeX -= box;
+    } else if(direction == "up"){
+        snakeY -= box;
+    } else if(direction == "down"){
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(startGame, 100);
