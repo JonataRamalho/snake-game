cbda68e5d766f1306ebccc79e903e35aca414bbf
JonataRamalho <jonataramalho.s@gmail.com>
2020-12-23 10:54:49 -0300

    feat: define fim do jogo

--- script.js
+++ script.js
@@ -83,6 +83,13 @@ function startGame(){
     } else if(snake[0].y < 0 && direction == "up"){
         snake[0].y = 16 * box;
     }
+
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert('Game Over :(');
+        }
+    }
 }
 
 let game = setInterval(startGame, 100);
